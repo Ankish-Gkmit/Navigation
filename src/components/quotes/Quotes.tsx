00a8dc89d27ac39f47ed39a33540dd78af0a3299
mac <mac@macs-MacBook-Pro.local>
2023-12-21 18:04:00 +0530

    Add styles and set loading

--- src/components/quotes/Quotes.tsx
+++ src/components/quotes/Quotes.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import {  useQuery  } from "@tanstack/react-query";
 import { SafeAreaView, Text, View } from "react-native";
 import SelectDropdown , {SelectDropdownProps} from "react-native-select-dropdown";
+import {styles} from './styles';
 
 const Quotes= ()=>{
 
@@ -75,20 +76,30 @@ if(isError)
 }
 
 return(
-    <View>
-            <Text>
-                Quotes based on 
-            </Text>
-            <SelectDropdown {...Props}/>
+    <View style={styles.container}>
+            <SelectDropdown {...Props} buttonStyle={{
+                backgroundColor: '#b5bfeb',
+                borderBlockColor: "black",
+                borderRadius: 10,
+                
+            }}
+                defaultButtonText="money"
+            />
             {
                 isLoading ? 
                 <Text>
                     Featching...
                 </Text>
                 :
-            <Text>  
+                <View>
+
+                <Text style={styles.author}>
+                    {data && data[0].author}
+                </Text>
+            <Text  style={styles.quote} >     
                 {data && data[0].quote}
             </Text>
+             </View>
             }
     </View>
 )}
---
+++ src/components/quotes/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        width:'auto',
+        flex :1, 
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    author :{
+        // width:'auto',
+        
+        padding:'auto',
+        margin: 10,
+        fontSize: 30,
+        color: 'black',
+        marginTop:20
+        
+    },
+    
+    quote:{
+        alignContent:'center',
+        padding: 5,
+        margin: 10
+    }
+
+})
